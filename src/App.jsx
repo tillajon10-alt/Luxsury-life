7f78afd9bec4feb86229cb26d52e293750721942
abduraxmatTillajon <tillajon10@gmail.com>
2026-06-22 14:32:01 +0500

    Loyiha boshlandi

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./App.css";
+
+const App = () => {
+  return (
+    <div>
+      <header>Header</header>
+      <main>main</main>
+      <footer>Footer</footer>
+    </div>
+  );
+};
+
+export default App;
